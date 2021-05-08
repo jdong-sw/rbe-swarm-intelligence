b398ae91ac0b8507c2d35c42c90ca9f412638837
addrick <asantos@wpi.edu>
2021-05-07 21:53:37 -0400

    sim class fully functional, runs sims in series. tracks explored % and # dead

--- TestSimulator.py
+++ TestSimulator.py
@@ -1,10 +1,4 @@
 from swarm_mapping.simulation import Simulation
-import cv2
-import numpy as np
-import importlib
-import sys
-importlib.reload(sys.modules['swarm_mapping.simulation'])
-
 
 # Display size
 display_width = 800
@@ -15,24 +9,7 @@ params = [[100, 3, 3],
           [200, 1, 1]]
 height = 100
 width = 100
-unexplored_thresh = .6
+explored_thresh = .8
 
-sim = Simulation(params, height, width, unexplored_thresh)
+sim = Simulation(params, height, width, explored_thresh)
 sim.start_sim()
-
-# while True:
-#     agents_map = world.update_agents_map()
-#     frame = world.render(agents_map)
-#     frame = cv2.resize(frame, (display_width, display_height), interpolation = cv2.INTER_AREA)
-#     cv2.imshow('Agent Map',cv2.cvtColor((frame*255).astype(np.uint8), cv2.COLOR_RGB2BGR))
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#     frame2 = world.render()
-#     frame2 = cv2.resize(frame2, (display_width, display_height), interpolation = cv2.INTER_AREA)
-#     cv2.imshow('Sim',cv2.cvtColor((frame2*255).astype(np.uint8), cv2.COLOR_RGB2BGR))
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#     world.step()
-#     step += 1
-# cv2.destroyAllWindows()
-
--- swarm_mapping/simulation.py
+++ swarm_mapping/simulation.py
@@ -2,15 +2,22 @@ import cv2
 from swarm_mapping.world import World
 import numpy as np
 
+# Map settings
 WIDTH = 100
 HEIGHT = 100
 SPACE_FILL = 0.5
 HAZ_FILL = 0.2
 
+# Display settings
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 800
+PRINT_FREQ = 50
 
-MAX_ITERATIONS = 100000
+# Sim settings
+MAX_ITERATIONS = 1000
+
+
+# TODO: some kind of controls for the maps, fixed seeds or something (maps are highly imbalanced)
 
 class Simulation:
     # takes a list of simulation parameters and runs them in succession (or parallel?)
@@ -22,37 +29,54 @@ class Simulation:
         self.explore_thresh = explore_thresh
         self.show_map = show_map
 
+        # Stats for analysis
+        self.explored_data = []
+        self.dead_data = []
+
+        # +6 is to match the border present on the Map() class
         self.size = (map_width+6)*(map_height+6)
 
     def start_sim(self):
         # run simulation for each line in parameters
+        # check every iteration and stop when any threshold is reached
         for sim_params in self.params:
+            print("Running new simulation...")
             num_agents = sim_params[0]
             marker_size = sim_params[1]
             sensor_range = sim_params[2]
             world = self.create_world(num_agents, marker_size, sensor_range)
             step = 0
-            unexplored = 1
-            # run sim until explored or TODO: other reasons: agents all die, nothing new explored for too long, etc
-            while unexplored >= self.explore_thresh\
+            explored = [0]
+            dead = [0]
+            # run sim until explored or until max_iterations is reached
+            while explored[-1] <= self.explore_thresh\
                     and step < MAX_ITERATIONS:
                 world.step()
-                # calculate percentage of the entire map that is unexplored
-                # TODO: maybe base on total empty spaces uncovered?
-                unexplored = np.count_nonzero(world.agents_map == -2) / self.size
-                print(unexplored)
+                step += 1
+                # calculate percentage of the entire map that is explored
+                explored.append(self.check_spaces_discovered(world))
+                dead.append(self.check_dead(world))
+
+                if step % PRINT_FREQ == 0:
+                    print("--------------")
+                    print("Step: " + str(step))
+                    print("Explored: " + str("{:.2f}". format(explored[-1]*100)) + "%")
+                    print("Dead: " + str(dead[-1]))
 
                 if self.show_map:
                     image = world.render()
                     shared_map = world.render(world.agents_map)
                     frame = np.concatenate((image, shared_map), axis=1)
                     frame = cv2.resize(frame, (DISPLAY_WIDTH*2, DISPLAY_HEIGHT), interpolation = cv2.INTER_AREA)
-                    cv2.imshow('Agent Map',cv2.cvtColor((frame*255).astype(np.uint8), cv2.COLOR_RGB2BGR))
+                    cv2.imshow('Simulation Map', cv2.cvtColor((frame*255).astype(np.uint8), cv2.COLOR_RGB2BGR))
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
+            self.explored_data.append(explored)
+            self.dead_data.append(dead)
+            print("=================================")
+            print("Simulation finished on step " + str(step))
+            print("Explored percentage: " + str("{:.2f}". format(explored[-1]*100)) + "%" + ", Dead: " + str(dead[-1]))
 
-                world.step()
-                step += 1
             cv2.destroyAllWindows()
 
     def create_world(self, num_agents, marker_size, sensor_range):
@@ -61,15 +85,15 @@ class Simulation:
                       sensor_range=sensor_range, marker_size=marker_size)
         return world
 
-    def check_discovered(self, world):
+    @staticmethod
+    def check_spaces_discovered(world):
         # check how many tiles the agents have revealed by checking how many squares != -2 ('unknown')
-        agent_map = world.agents_map
-        discovered_cells = agent_map.count(-2)
-        discovered = discovered_cells / self.size
-        return discovered
+        discovered_empty_cells = np.count_nonzero(world.agents_map == 0)
+        total_empty_cells = np.count_nonzero(world.map.grid == 0)
+        return discovered_empty_cells / total_empty_cells
 
     @staticmethod
-    def check_dead(self, world):
+    def check_dead(world):
         # returns number of agents dead
         agents_total = len(world.agents)
         agents_dead = 0
@@ -79,7 +103,3 @@ class Simulation:
                 agents_dead += 1
         return agents_dead
 
-# TODO: create a loop to generate a World for each parameter set
-        # check_discovered every iteration and stop when threshold is reached, then calculate statistics
-
-    # calculate/record various statistics about the run:
